a155527b56806ef98b6c997c97b8b311dfc4048e
ampatspell <ampatspell@gmail.com>
2021-02-23 21:46:21 +0200

    css

--- app/components/route/dev.js
+++ app/components/route/dev.js
@@ -1,13 +1,4 @@
 import Component from '@glimmer/component';
-import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 
 export default class RouteDevComponent extends Component {
-
-  @service store;
-
-  @action
-  async onClick() {
-  }
-
 }
