a7c1937d83c7698b282002991177fba48dc79aba
ampatspell <ampatspell@gmail.com>
2021-02-25 21:09:34 +0200

    nicer handling of missing identifiers

--- app/components/block/project/inspector/content/reference.js
+++ app/components/block/project/inspector/content/reference.js
@@ -2,13 +2,33 @@ import Component from '@glimmer/component';
 import { action } from "@ember/object";
 import { reads } from "macro-decorators";
 
+class Missing {
+
+  missing = true;
+
+  constructor(component) {
+    this._component = component;
+  }
+
+  get identifier() {
+    return `Missing "${this._component.reference.identifier}"`;
+  }
+
+}
+
 export default class BlockProjectInspectorContentReferenceComponent extends Component {
 
   @reads('args.model.nodes.identified') identified;
 
+  missing = new Missing(this);
+
   get values() {
-    let { args: { collection } } = this;
-    return [ null, ...this.identified[collection] ];
+    let { args: { collection }, value } = this;
+    let models = this.identified[collection];
+    if(value?.missing) {
+      return [ null, value, ...models ];
+    }
+    return [ null, ...models ];
   }
 
   get reference() {
@@ -16,10 +36,19 @@ export default class BlockProjectInspectorContentReferenceComponent extends Comp
     return model[key];
   }
 
-  @reads('reference.model') value;
+  get value() {
+    let { reference: { model, missing } }  = this;
+    if(missing) {
+      return this.missing;
+    }
+    return model;
+  }
 
   @action
   onChange(value) {
+    if(value?.missing) {
+      return;
+    }
     let { args: { model, key } } = this;
     model.update({ [key]: value });
   }
