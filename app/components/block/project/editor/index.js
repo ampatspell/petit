501b014b6b8f1183fd897f67b373ed1a482e64e7
ampatspell <ampatspell@gmail.com>
2021-02-26 18:45:25 +0200

    deselect

--- app/components/block/project/editor/index.js
+++ app/components/block/project/editor/index.js
@@ -4,6 +4,19 @@ import { action } from "@ember/object";
 export default class BlockProjectEditorIndexComponent extends Component {
 
   @action
+  didInsertContent(el) {
+    this.contentElement = el;
+  }
+
+  @action
+  onDeselect(e) {
+    if(e.target !== this.contentElement) {
+      return;
+    }
+    this.onSelect(null);
+  }
+
+  @action
   onSelect(node) {
     this.args.project.nodes.select(node);
   }
