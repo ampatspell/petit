e92b68d4bf005ad29ce8cd61e4ba491a3a1bf552
ampatspell <ampatspell@gmail.com>
2021-03-02 06:00:10 +0200

    messing with stuff

--- app/components/block/project/editor/node/scene/index.js
+++ app/components/block/project/editor/node/scene/index.js
@@ -3,11 +3,18 @@ import { action } from "@ember/object";
 import { reads, equal } from "macro-decorators";
 import { stroke } from 'petit/util/stroke';
 
+let visible = arr => arr.filter(node => !node.hide.hidden);
+
 export default class BlockProjectEditorNodeSceneIndexComponent extends Component {
 
   @reads('args.node') scene;
   @equal('scene.tools.selected.type', 'edit') editing;
   @equal('scene.tools.selected.type', 'resize') resizing;
+
+  // TOOD: Move to -node
+  get layers() {
+    return visible(this.scene.layers).reverse();
+  }
 
   get size() {
     let { width, height, pixel: { absolute: pixel } } = this.scene;
