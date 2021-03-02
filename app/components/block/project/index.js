e7e4997804b21348a25c1ea9e1ce1a6d86625a3e
ampatspell <ampatspell@gmail.com>
2021-03-02 23:27:13 +0200

    Update index.js

--- app/components/block/project/index.js
+++ app/components/block/project/index.js
@@ -20,30 +20,30 @@ export default class BlockProjectIndexComponent extends Component {
     await this.project.delete();
   }
 
-  @action
-  onBindHotkeys(hotkeys) {
-    let wrap = cb => e => {
-      if(e.target.tagName !== 'INPUT' && !e.metaKey) {
-        cb();
-      }
-    };
+  // @action
+  // onBindHotkeys(hotkeys) {
+  //   let wrap = cb => e => {
+  //     if(e.target.tagName !== 'INPUT' && !e.metaKey) {
+  //       cb();
+  //     }
+  //   };
 
-    let forward = name => wrap(() => {
-      let project = this.project;
-      project[name].call(project);
-    });
+  //   let forward = name => wrap(() => {
+  //     let project = this.project;
+  //     project[name].call(project);
+  //   });
 
-    hotkeys.add('esc', forward('onKeyEsc'));
-    hotkeys.add('left', forward('onKeyLeft'));
-    hotkeys.add('right', forward('onKeyRight'));
+  //   hotkeys.add('esc', forward('onKeyEsc'));
+  //   hotkeys.add('left', forward('onKeyLeft'));
+  //   hotkeys.add('right', forward('onKeyRight'));
 
-    [ 'e', 'r' ].forEach(key => {
-      hotkeys.add(key, wrap(() => this.project.onKeyLetter(key)));
-    });
+  //   [ 'e', 'r' ].forEach(key => {
+  //     hotkeys.add(key, wrap(() => this.project.onKeyLetter(key)));
+  //   });
 
-    [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ].forEach(key => {
-      hotkeys.add(`${key}`, wrap(() => this.project.onKeyNumber(key)));
-    });
-  }
+  //   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ].forEach(key => {
+  //     hotkeys.add(`${key}`, wrap(() => this.project.onKeyNumber(key)));
+  //   });
+  // }
 
 }
