337c689145a9446467d6b44836aea9ccbabbd6ee
ampatspell <ampatspell@gmail.com>
2021-03-09 01:23:13 +0200

    Update colors.js

--- app/models/project/node/sprite/colors.js
+++ app/models/project/node/sprite/colors.js
@@ -4,6 +4,9 @@ class Colors {
   constructor(node, opts) {
     this.node = node;
     this.opts = opts;
+    if(!this.data) {
+      node.update({ [this.opts.key]: [] });
+    }
   }
 
   get palette() {
@@ -11,7 +14,40 @@ class Colors {
   }
 
   get data() {
-    return this.node.doc.data[this.opts.key] || [];
+    return this.node.doc.data[this.opts.key];
+  }
+
+  _nextInt() {
+    let { data } = this;
+    let value = 0;
+    // eslint-disable-next-line no-constant-condition
+    while(true) {
+      if(!data.find(item => item.value === value)) {
+        return value;
+      } else {
+        value++;
+      }
+    }
+  }
+
+  _hashForColor(color) {
+    let { identifier } = color;
+    let { data } = this;
+    return data.find(hash => hash.identifier === identifier);
+  }
+
+  _addHash(hash) {
+    this.data.push(hash);
+    this.node._scheduleSave.schedule();
+  }
+
+  register(color) {
+    if(this._hashForColor(color)) {
+      return;
+    }
+    let { identifier } = color;
+    let value = this._nextInt();
+    this._addHash({ identifier, value });
   }
 
 }
