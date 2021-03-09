67b003ea0b4d38bd1532451c26ae711602361af2
ampatspell <ampatspell@gmail.com>
2021-03-09 20:51:41 +0200

    border color for selected

--- app/components/block/editor/border.js
+++ app/components/block/editor/border.js
@@ -4,23 +4,33 @@ import { reads, equal } from "macro-decorators";
 export default class BlockEditorBorderComponent extends Component {
 
   @reads('args.node') node;
+  @reads('node.nodes.selected.group') selected;
   @reads('node.tools.selected') tool;
   @equal('tool.type', 'edit') editing;
   @equal('tool.overlaysHidden', true) overlaysHidden;
   @reads('node.lock.locked') locked;
 
   get state() {
-    let { overlaysHidden, locked, editing } = this;
+    let { overlaysHidden, locked, editing, node, selected } = this;
+    let classes = [];
+
+    selected = node === selected;
+
+    if(selected) {
+      classes.push('selected');
+    }
+
     if(overlaysHidden) {
-      return 'hidden';
+      classes.push('hidden');
+    } else if(editing) {
+      classes.push('editing');
+    } else if(locked) {
+      classes.push('locked');
+    } else {
+      classes.push('regular');
     }
-    if(locked) {
-      return 'locked';
-    }
-    if(editing) {
-      return 'editing';
-    }
-    return 'regular';
+
+    return classes.join(' ');
   }
 
 }
