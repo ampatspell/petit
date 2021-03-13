16551f6415d0baf23a94fa58bd2303a9b1c183dc
ampatspell <ampatspell@gmail.com>
2021-03-13 03:09:29 +0200

    Update sequence.js

--- app/components/block/project/tools/sequence.js
+++ app/components/block/project/tools/sequence.js
@@ -8,6 +8,14 @@ export default class BlockProjectToolsSequenceComponent extends Base {
     keys.add('left', handler(() => this.node.selection.prev()));
     keys.add('right', handler(() => this.node.selection.next()));
     keys.add('c', handler(() => this.node.editor.actions.center()));
+    keys.add('esc', handler(() => {
+      let { node, tools, tool } = this;
+      if(tool.type === 'idle') {
+        node.nodes.select(node);
+      } else {
+        tools.reset();
+      }
+    }));
     [ 1, 2, 4, 8 ].forEach((pixel, idx) => {
       keys.add(`alt + ${idx+1}`, handler(() => {
         this.node.update({ pixel });
