233619cca14d8023657bd36bcdf72e18d531316b
ampatspell <ampatspell@gmail.com>
2021-03-16 16:06:05 +0200

    bad dragging

--- app/components/block/project/editor/node/scene/layer/entity.js
+++ app/components/block/project/editor/node/scene/layer/entity.js
@@ -1,5 +1,5 @@
 import Group from 'ember-cli-konva/components/konva/node/group';
-import { reads } from "macro-decorators";
+import { reads, equal } from "macro-decorators";
 import { cached } from "tracked-toolbox";
 
 export default class BlockProjectEditorNodeSceneLayerEntityComponent extends Group {
@@ -7,6 +7,8 @@ export default class BlockProjectEditorNodeSceneLayerEntityComponent extends Gro
   @reads('args.node') entity;
   @reads('entity.scene') scene;
   @reads('scene.pixel.absolute') pixel;
+
+  @equal('scene.tools.selected.type', 'edit') editing;
 
   @cached
   get size() {
@@ -32,9 +34,59 @@ export default class BlockProjectEditorNodeSceneLayerEntityComponent extends Gro
   }
 
   didCreateNode() {
-    this.on('mousedown', () => {
+
+    let dragging = null;
+
+    let getPointerPosition = ({ evt: e }) => {
+      let { pageX: x, pageY: y } = e;
+      return { x, y };
+    }
+
+    this.on('mousedown', e => {
       let { entity, entity: { nodes } } = this;
       nodes.select(entity);
+
+      if(!this.editing) {
+        return;
+      }
+
+      let pointer = getPointerPosition(e);
+      let { x, y } = entity.editor;
+
+      dragging = {
+        pointer,
+        position: { x, y }
+      };
+    });
+
+    this.on('mousemove', e => {
+      if(!dragging) {
+        return;
+      }
+
+      let pointer = getPointerPosition(e);
+      let { entity, scene: { pixel: { absolute: pixel } } } = this;
+
+      let round = v => Math.ceil(v / pixel) * pixel;
+
+      let _diff = prop => round(pointer[prop] - dragging.pointer[prop]);
+      let diff = {
+        x: _diff('x'),
+        y: _diff('y')
+      };
+
+      let _c = prop => Math.ceil((dragging.position[prop] + diff[prop]) / pixel);
+      entity.update({
+        x: _c('x'),
+        y: _c('y'),
+      });
+    });
+
+    this.on('mouseup', () => {
+      if(!dragging) {
+        return;
+      }
+      dragging = null;
     });
   }
 
