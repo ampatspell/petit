8c6c4e281784206385cceda00ca4dface05612d8
ampatspell <ampatspell@gmail.com>
2021-03-16 13:42:07 +0200

    cleanup

--- app/components/block/project/editor/node/scene/layer/entity/-base.js
+++ app/components/block/project/editor/node/scene/layer/entity/-base.js
@@ -1,23 +1,11 @@
 import Group from 'ember-cli-konva/components/konva/node/group';
 import { reads } from "macro-decorators";
-import { cached } from "tracked-toolbox";
 
 export default class EntityComponent extends Group {
 
   @reads('args.node') entity;
   @reads('entity.scene') scene;
   @reads('scene.pixel.absolute') pixel;
-
-  @cached
-  get hitFunc() {
-    return (context, shape) => {
-      let { width, height } = shape.size();
-      context.beginPath();
-      context.rect(0, 0, width, height);
-      context.closePath();
-      context.fillStrokeShape(shape);
-    };
-  }
 
   get nodeProperties() {
     let { entity: { x, y }, pixel } = this;
@@ -26,6 +14,12 @@ export default class EntityComponent extends Group {
       x: s(x),
       y: s(y)
     };
+  }
+
+  didCreateNode() {
+    this.on('mousedown', () => {
+      console.log('entity');
+    });
   }
 
 }
