716f4e2e62b569b4de2945b2eae6c9f6e24480cf
ampatspell <ampatspell@gmail.com>
2021-03-16 15:47:54 +0200

    generalize shortcuts a bit

--- app/components/block/project/tools/-base.js
+++ app/components/block/project/tools/-base.js
@@ -9,6 +9,55 @@ export const handler = cb => e => {
   return cb(e);
 }
 
+export const arrows = (component, keys) => {
+  let delta = (dx, dy) => handler(() => {
+    let { node, node: { x, y } } = component;
+    node.update({ x: x + dx, y: y + dy });
+  });
+  keys.add('shift + up', delta(0, -1));
+  keys.add('shift + down', delta(0, 1));
+  keys.add('shift + left', delta(-1, 0));
+  keys.add('shift + right', delta(1, 0));
+}
+
+export const center = (component, keys) => {
+  keys.add('c', handler(() => component.node.editor.actions.center()));
+}
+
+export const edit = (component, keys) => {
+  keys.add('e', handler(() => component.tools.selectByType('edit')));
+}
+
+export const resize = (component, keys) => {
+  keys.add('r', handler(() => component.tools.selectByType('resize')));
+}
+
+export const reset = (component, keys) => {
+  keys.add('esc', handler(() => {
+    let { node, tools, tool } = component;
+    if(tool.type === 'idle') {
+      node.nodes.select(node);
+    } else {
+      tools.reset();
+    }
+  }));
+}
+
+export const pixel = (component, keys, values=[ 1, 2, 4, 8 ]) => {
+  values.forEach((pixel, idx) => {
+    keys.add(`alt + ${idx+1}`, handler(() => component.node.update({ pixel })));
+  });
+}
+
+export const drag = (component, keys) => {
+  keys.add('space', handler(e => {
+    e.preventRepeat();
+    component.node.tools.selectByType('project:drag');
+  }), handler(() => {
+    component.node.tools.reset();
+  }));
+}
+
 export default class BlockProjectToolsBaseComponent extends Component {
 
   @reads('args.node') node;
